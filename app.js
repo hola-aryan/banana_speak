6dd04dfb4131bae66b1b67abc6260690c955922e
hola-aryan <aryanisastudent@gmail.com>
2022-11-23 20:58:13 +0530

    refactor: banana

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-// // Input and output on website
-
-// console.log("Script is working in diffent file");
-// var username = prompt("Give me your username");
-// alert("This Script works "+username);
-
 var btnTranslate = document.querySelector("#btn-translate");
 var txtarea = document.querySelector("#txtarea");
+var outputDiv = document.querySelector("#output");
 
-// console.log(btnTranslate);
+outputDiv.innerText="Tanay Pratap";
 
 btnTranslate.addEventListener("click",function clickeventhandler() {
     console.log("clicked");
 })
 btnTranslate.addEventListener("click",function valTxtArea() {
     console.log("input "+ txtarea.value);
-})
+ })
+
+// // Input and output on website
+
+// console.log("Script is working in diffent file");
+// var username = prompt("Give me your username");
+// alert("This Script works "+username);
+
+
+//  For challenge
+// var txtarea = document.querySelector("textarea");
+// var txtarea = document.querySelector(".btn-primary");
+// var txtarea = document.querySelector("#input-btn");
+// var txtarea = document.querySelector("input[name='translator']");
+
+// console.log(btnTranslate);
